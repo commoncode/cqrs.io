a26b4f9d0982c37a3b790a6471e9f63b1572d661
Dipak Yadav <dipak.kumar@clavax.com>
2014-05-06 19:11:19 +0530

    add to cart functionality and operations log.

--- skeleton2/meteorapp/meteorapp.js
+++ skeleton2/meteorapp/meteorapp.js
@@ -1,46 +1,55 @@
 Products = new Meteor.Collection("products", {idGeneration:'MONGO'});
+Cart = new Meteor.Collection("cart", {idGeneration:'MONGO'});
+Operationslog = new Meteor.Collection("operationslog", {idGeneration:'MONGO'});
 
 if (Meteor.isClient) {
   Meteor.subscribe("products");
+  Meteor.subscribe("cart");
+  Meteor.subscribe("operations");
+  
   Template.productlist.products = function() {
       return Products.find();
   };
   
-  /*
-  Template.hello.greeting = function () {
-    return "Welcome to meteorapp.";
+  Template.cartlist.cproducts = function() {
+      return Cart.find({owner:Meteor.userId()});
   };
+  
+  Template.operationlog.cqrslog = function() {
+      return Operationslog.find();
+  };
+  
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-  */
   
   Template.product.events({
       'click .btnAddToCart': function() {
-          console.log(this);
+          if(Cart.find({name:this.name}).count() == 0) {
+              Operationslog.insert({description:"Add to cart for product : " + this.name, productname: this.name, status: "Initiated", userid: Meteor.userId()});
+              Cart.insert({name:this.name, owner: Meteor.userId()});
+              //Operations.update({productname:this.name, userid: Meteor.userId()}, { $set: { status: "Complete" } });
+          } else {
+              alert("already in cart");
+          }
       }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+      
     // code to run on server at startup
+    /*
     if(Products.find().count() === 0) {
         var products = [
             {
                 name: "Mobile",
                 description: "A Slim Mobile phone",
-                price: "$ 400"
+                price: "400"
             },
             {
                 name: "Charger",
                 description: "A thin Charger for Mobile phone",
-                price: "$ 500"
+                price: "500"
             }
         ];
         
@@ -48,5 +57,6 @@ if (Meteor.isServer) {
             Products.insert({name: products[i].name, description: products[i].description, price: products[i].price});
         }
     }
+    */
   });
 }
